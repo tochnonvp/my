7af5755b9050f9c17bc1ce2476d339f0783d9e55
Firstname Lastname <tochno.nvp@gmail.com>
2024-01-29 22:02:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите операцию со знаками +,-,*,/, \nиспользуйте числа от 1 до 10 включительно");
+            String input = scanner.next();
+            String[] strings = input.split("\\W");
+            int a = Integer.parseInt(strings[0]);
+            int b = Integer.parseInt(strings[1]);
+
+            if (b>= 11 | a >= 11) {
+                System.out.println("не выполнено усовение задачи");
+            } else if (input.contains("+")) {
+                System.out.println(a + b);
+            } else if (input.contains("*")) {
+                System.out.println(a * b);
+            } else if (input.contains("-")) {
+                System.out.println(a - b);
+            } else if (input.contains("/")) {
+                System.out.println(a / b);
+            } else {
+                System.out.println("не выполнено условие задачи");
+            }
+
+        } catch (Exception e) {
+            System.out.println("не выполнено условие задачи");//один два
+        }
+
+
+    }
+}
